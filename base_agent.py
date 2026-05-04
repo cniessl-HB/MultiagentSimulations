19294db6a39955b060f457c8fd4b2faf9a75d0c8
Chris Niessl <niessl@hellbender.com>
2026-05-03 21:54:54 -0400

    feat: base_agent.py - Add in base agent

---
+++ base_agent.py
@@ -0,0 +1,30 @@
+"""
+Agent.py - Common interface class for individual agents
+
+Copyright 2026 - Christopher T Niessl
+
+See LICENSE.txt for usage.
+"""
+
+class base_agent:
+    """Placeholder for base agent.
+    
+    Agents have an ID, state, and maintain a history of
+    interactions.
+    
+    """ 
+    
+    def __init__(self) -> None:
+        "Initialize the agent"
+        self.agent_id: str = ""
+        self.agent_state: any = None
+        self.possible_interacts: list = []
+        self.history_list: list = []
+    
+    def interact(self, other: base_agent) -> None:
+        "Override this function with interaction method"
+        raise NotImplemented
+    
+    def audit_history(self) -> list:
+        "Return the history of interactions"
+        return self.history_list
