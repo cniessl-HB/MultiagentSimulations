e1577391060128bffa3c7f14dfcde2d1144c52dd
Christopher T Niessl <cniessl_dev@proton.me>
2026-08-08 20:22:27 -0400

    feat: begin adding in tls connectivity

--- src/basedistdimlib/dist_resource_info.py
+++ src/basedistdimlib/dist_resource_info.py
@@ -6,21 +6,28 @@ Copyright 2026 - Christopher T Niessl
 See LICENSE.txt for usage.
 """
 
+import socket
+import ssl
+
 class dist_resource_info(base_dist_resc):
     """Distributed resource info class."""
     
     def __init__(self, 
                  resc_name: str, 
-                 ip_addr: str, 
-                 port: int,
-                 auth_ket: str,
+                 target_host: str, 
+                 target_port: int,
+                 auth_cert: str,
                  require_online = False):
         self.resc_name: str = resc_name
         self.ip_addr: str = ip_addr
         self.port: int = port
-        self.auth_key: str = auth_key
+        self.auth_cert: str = auth_cert
         self.require_online: bool = require_online
     
     def connect(self) -> bool:
         """Attempt handshake connection with resource."""
-        raise NotImplementedError
+        self.tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+        unwrapped_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.secure_socket = context.wrap_socket(raw_socket, server_hostname='localhost') as secure_socket:
+        return self.secure_socket.connect((target_host, target_port))
+
