c21e395e5ce6d50baac52e83cb573c48870948dc
Christopher T Niessl <cniessl_dev@proton.me>
2026-08-14 22:20:01 -0400

    feat: begin adding in dist agent uuid infrastructure

--- src/basedistdimlib/dist_sim_agent.py
+++ src/basedistdimlib/dist_sim_agent.py
@@ -6,8 +6,26 @@ Copyright 2026 - Christopher T Niessl
 See LICENSE.txt for usage.
 """
 
+from basesimlib.base_masim_types import (
+    some_action_stub,
+    some_agent,
+    some_log,
+)
+
 class dist_sim_agent(some_agent):
     """Base distributed agent."""
+
+    def __init__(self, agent_id: str, host_sim_name) -> None:
+        "Initialize the agent"
+        self.agent_id: str = agent_id
+        self.active: bool = True
+        self.possible_actions: dict[str, callable] = {}
+        self.next_action: str | None = None
+        self.history_list: list[some_log] = []
+        self.uuid: uuid.UUID = self._get_sim_uuid(host_sim_name)
+
+    def _get_sim_uuid(self, host_sim_name) -> uuid.UUID
+        raise NotImplementedError
 
     def get_id(self) -> str:
         raise NotImplementedError
