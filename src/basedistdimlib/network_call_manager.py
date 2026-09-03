524f1dacf2af9429c19e5a288d092bc284b0f044
Christopher T Niessl <cniessl_dev@proton.me>
2026-09-02 22:36:22 -0400

    feat: continue adding functionality to network_call_manager.py

--- src/basedistdimlib/network_call_manager.py
+++ src/basedistdimlib/network_call_manager.py
@@ -8,6 +8,8 @@ See LICENSE.txt for usage.
 
 import threading
 
+from time import sleep
+
 class network_call_manager():
 
     def __init__():
@@ -15,12 +17,37 @@ class network_call_manager():
         self._task_notifiers = {}
         self._outstanding_tasks = {}
         self._completed_tasks = {}
+        self._shutdown_notifier = threading.Event()
+        
+        self._is_running = False
+        self._ready = False
+        self._msg_handler = None
+    
+    def is_running(self):
+        with self._task_manager_mutex:
+            return self._is_running
     
     def setup_connection():
         raise NotImplementedError
     
-    def setup_task():
+    def setup_task(self):
         raise NotImplementedError
     
-    def _main_listen_loop():
-        raise NotImplementedError
+    def start(self):
+        if not self._ready:
+            raise RuntimeException
+        self._is_running = True
+        self._msg_handler = threading.Thread(target=self._main_listen_loop(), daemon=True)
+        self._msg_handler.start()
+        
+    def stop(self):
+        with self._task_manager_mutex:
+            self._is_running = False
+        if self._msg_handler:
+            self._msg_handler.join() 
+       
+    def _main_listen_loop(self):
+        
+        while self.is_running():
+            sleep(1)
+        
