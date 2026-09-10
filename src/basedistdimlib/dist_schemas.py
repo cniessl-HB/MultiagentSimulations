066f6eaad411bcb3e3e8e59d6e7b0a0bc315f3e9
Christopher T Niessl <cniessl_dev@proton.me>
2026-09-09 22:37:38 -0400

    feat: add schemas for RPC/messages/tasks to be sent between peers

---
+++ src/basedistdimlib/dist_schemas.py
@@ -0,0 +1,15 @@
+"""
+task_schemas.py - JSON Schemas for various RPC/peer-tasks.
+
+Copyright 2026 - Christopher T Niessl
+
+See LICENSE.txt for usage.
+"""
+
+get_resource_at_key_schema = {}
+
+pass_resource_to_peer_schema = {}
+
+get_peer_network_info = {}
+
+find_next_available_key = {}
