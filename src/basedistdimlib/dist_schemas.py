7762a86218dd7fecd321594c950e9abf1963cb29
Christopher T Niessl <cniessl_dev@proton.me>
2026-09-15 22:16:16 -0400

    feat: Add magic number scanner for packets

--- src/basedistdimlib/dist_schemas.py
+++ src/basedistdimlib/dist_schemas.py
@@ -14,7 +14,7 @@ get_peer_network_info = {}
 
 find_next_available_key = {}
 
-SCHEMA_HEADER_MAGIC_NUM = 0x4d53696d
+SCHEMA_HEADER_MAGIC_NUM = b'\x4d\x53\x69\x6d'
 
 class schema_header():
 
@@ -22,10 +22,35 @@ class schema_header():
         self.total_size = total_size
         self.text_checksum = text_checksum
 
-def scan_for_start(self, input_bytes: bytes) -> int:
-    for ii in range(0, len(input_bytes) - 4):
-        if input_bytes[ii:(ii+4)] == SCHEMA_HEADER_MAGIC_NUM:
-            return ii
+class packet_state(Enum):
+    SCANNING_MN = 1
+    SCANNING_SIZE = 2
+    SCANNING_CHKSUM = 3
+    READING_DATA = 4
+
+class packet_scanner():
+
+    def __init__(self):
+        self.state = packet_state.SCANNING_MN
+        self.current_header = schema_header(0, b'\x00\x00\x00\x00')
+        self.bytes_in_state = 0
+        self.buffer = b''
+
+    def scan_and_process(self, input_bytes: bytes):
+        trun_bytes = b''
+        if self.state == packet_state.SCANNING_MN:
+            ret_val = self._scan_magic_number(input_bytes)
+            if ret_val > 0:
+            trun_bytes = input_bytes[ret_val:]
+
+    def _scan_magic_number(self, input_bytes: bytes) -> int:
+        for ii in range(0, len(input_bytes)):
+            if input_bytes[ii] == SCHEMA_HEADER_MAGIC_NUM[self.bytes_in_state]:
+                self.bytes_in_state += 1
+            if self.bytes_in_state == 4:
+                return ii+1
+        return -1
+            
     return -1
     # TODO: Change this to be more robust to partial packets
 
